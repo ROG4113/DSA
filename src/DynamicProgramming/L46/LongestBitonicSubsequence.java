f93132b8ea6c070d9eb85b2964eefcb3da1a029a
ROG4113 <as0009916@gmail.com>
2026-04-22 14:19:36 +0530

    adding DP lecture-46

---
+++ src/DynamicProgramming/L46/LongestBitonicSubsequence.java
@@ -0,0 +1,39 @@
+package DynamicProgramming.L46;
+
+import java.util.Arrays;
+
+public class LongestBitonicSubsequence {
+    public static int lbs(int[] nums){
+        // two LIS one ffrom front one from back dp1[i]+dp2[i]-1, -1 because middle one will be common
+        int[] dp1=new int[nums.length];
+        Arrays.fill(dp1, 1);
+        for(int ind=0; ind<nums.length; ind++){
+            for(int prevInd=0; prevInd<ind; prevInd++){
+                if(nums[prevInd]<nums[ind] && dp1[ind]<dp1[prevInd]+1){
+                    dp1[ind]=dp1[prevInd]+1;
+                }
+            }
+        }
+        // from back
+        int[] dp2=new int[nums.length];
+        Arrays.fill(dp2, 1);
+        for(int ind=nums.length-1; ind>=0; ind--){
+            for(int prevInd=nums.length-1; prevInd>ind; prevInd--){
+                if(nums[prevInd]<nums[ind] && dp2[ind]<dp2[prevInd]+1){
+                    dp2[ind]=dp2[prevInd]+1;
+                }
+            }
+        }
+
+        int max=1;
+        for(int i=0; i<nums.length; i++){
+            max=Math.max(max, dp1[i]+dp2[i]-1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums=new int[]{5, 1, 4, 2, 3, 6, 8, 7};
+        System.out.println(lbs(nums));
+    }
+}
